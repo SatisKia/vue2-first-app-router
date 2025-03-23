62734518963bc73e44e9e9607cdd4a00c31326e2
SatisKia <satis@msd.biglobe.ne.jp>
2025-03-24 07:18:52 +0900

    initial commit

---
+++ src/store/index.ts
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { Todo } from '@/types/todo'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    todoList: [] as Todo[]
+  },
+  getters: {
+    todoList (state) {
+      return state.todoList
+    }
+  },
+  mutations: {
+    initialize (state) {
+      state.todoList = [] as Todo[]
+    },
+    push (state, payload: { todo: Todo }) {
+      state.todoList.push(payload.todo)
+    },
+    add (state, payload: { todo: Todo }) {
+      state.todoList = [...state.todoList, payload.todo]
+    },
+    remove (state, payload: { id: string }) {
+      state.todoList = state.todoList.filter(todo => todo.id !== payload.id)
+    },
+    done (state, payload: { id: string }) {
+      const todoList = state.todoList.slice()
+      const todo = todoList.find(todo => todo.id === payload.id)
+      if (todo) {
+        todo.done = !todo.done
+        state.todoList = todoList
+      }
+    }
+  },
+  actions: {
+    initialize (context) {
+      context.commit('initialize')
+    },
+    push (context, payload: { todo: Todo }) {
+      context.commit('push', payload)
+    },
+    add (context, payload: { todo: Todo }) {
+      context.commit('add', payload)
+    },
+    remove (context, payload: { id: string }) {
+      context.commit('remove', payload)
+    },
+    done (context, payload: { id: string }) {
+      context.commit('done', payload)
+    }
+  }
+})
